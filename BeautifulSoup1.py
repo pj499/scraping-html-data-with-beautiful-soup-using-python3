d791d268a594bea02a418bd1776e46e309561bff
Prajakta Pikale <43418085+pj499@users.noreply.github.com>
2020-06-28 03:23:14 +0530

    Update BeautifulSoup1.py

--- BeautifulSoup1.py
+++ BeautifulSoup1.py
@@ -1,14 +1,21 @@
+#==========Import required libraries==========
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 
+#==========Enter URL==========
 name=input("Enter:")
+
+#==========Connect the contents in HTML page in handler==========
 content=urllib.request.urlopen(name).read()
 
-sum=0
+#==========Organize the messy content of HTML page==========
 soup=BeautifulSoup(content,'html.parser')
 
+#==========Find the span tag==========
 tags=soup('span')
 
+#==========Evaluate sum==========
+sum=0
 for tag in tags:
     num=tag.contents
     n=num[0]
